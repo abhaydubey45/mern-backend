39ff8b38226b7563427a153a55b1f85a1128433d
Abhay Dubey <u22ch021@ched.svnit.ac.in>
2024-01-11 21:42:30 +0530

    Add videofile, thumbnail, description, duration, views, isPublished, and owner fields to VideoSchema

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,37 @@
 import mongoose from 'mongoose'
 
 const VideoSchema = new mongoose.Schema({
-    
+    videofile:{
+        type: String,
+        required:true
+    },
+    thumbnail:{
+        type:String, //cloudanary url
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:String,    
+        required:true
+    },
+    views:{ 
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'User'
+    }
+    }
+
+,{
+    timestamps: true
 })
