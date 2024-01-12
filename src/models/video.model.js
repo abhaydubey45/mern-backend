2db97ccc9f12c981d6fe66b16af9974cdcdd6463
Abhay Dubey <u22ch021@ched.svnit.ac.in>
2024-01-12 22:14:04 +0530

    Add bcrypt, jsonwebtoken, and mongoose-aggregate-paginate-v2 dependencies, and implement password hashing and token generation in user model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
 
 const VideoSchema = new mongoose.Schema({
     videofile:{
@@ -35,3 +36,6 @@ const VideoSchema = new mongoose.Schema({
 ,{
     timestamps: true
 })
+mongoose.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model('Video', VideoSchema);
